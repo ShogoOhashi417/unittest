0cf7a68fdbb9f0dac961acb463c25bef289b7d32
shogo <shogo.ohashi0417@gmail.com>
2024-07-20 15:21:38 +0900

    good

--- test_practice/src/UsageFee.php
+++ test_practice/src/UsageFee.php
@@ -4,61 +4,38 @@ namespace App;
 
 class UsageFee {
 
-    // ユーザー課金とオプション課金の合計
-    public function getTotalFee(
-        $userNumber,
-        $optionNumber
-    ){
-        $result = 0;
-
-        // ユーザー数が5人以下なら一律5000円
-        // 50人以下なら、1ユーザー1000円
-        // 50を超える分は1ユーザー500円
+    public function getUserFee($userNumber)
+    {
         if ($userNumber <= 5) {
-            $result += 5000;
-        } elseif ($userNumber <= 50) {
-            $result += 1000 * $userNumber;
-        } else {
-            $result += 1000 * 50 + 500 * ($userNumber - 50);
+            return 5000;
         }
-
-        // オプション数が3以下なら、1オプション25000円
-        // ４以上なら、1オプション20000円
-        if ($optionNumber <= 3) {
-            $result += 25000 * $optionNumber;
-        } else {
-            $result += 20000 * $optionNumber;
+        
+        if ($userNumber <= 50) {
+            return 1000 * $userNumber;
         }
-
-        return $result;
+        
+        return 1000 * 50 + 500 * ($userNumber - 50);
     }
 
-    // 初期費用に対するユーザー課金料金の割合
-    public function getUserFeeRatio(
-        $userNumber,
-        $contractYear
-    ){
-        $userFee = 0;
-
-        // ユーザー数が5人以下なら一律5000円
-        // 50人以下なら、1ユーザー1000円
-        // 50を超える分は1ユーザー500円
-        if ($userNumber <= 5) {
-            $userFee += 5000;
-        } elseif ($userNumber <= 50) {
-            $userFee += 1000 * $userNumber;
-        } else {
-            $userFee += 1000 * 50 + 500 * ($userNumber - 50);
+    public function getOptionFee($optionNumber)
+    {
+        if ($optionNumber < 0) {
+            return 0;
+        }
+        
+        if ($optionNumber <= 3) {
+            return 25000 * $optionNumber;
         }
 
-        $initialCost = 0;
+        return 20000 * $optionNumber;
+    }
 
+    public function getInitialCost($contractYear)
+    {
         if ($contractYear >= 2024) {
-            $initialCost = 300000;
-        } else {
-            $initialCost = 100000;
+            return 300000;
         }
 
-        return $userFee / $initialCost;
+        return 100000;
     }
 }
--- test_practice/test/UsageFeeTest.php
+++ test_practice/test/UsageFeeTest.php
@@ -4,40 +4,43 @@ use App\UsageFee;
 use PHPUnit\Framework\TestCase;
 
 class UsageFeeTest extends TestCase {
+    // protected UsageFee $usageFee;
 
-    public function test_Sum_UsageFee_ReturnTotalFee()
+    // protected function setUp(): void
+    // {
+    //     $this->usageFee = new UsageFee();
+    // }
+
+    public function testUserFee()
     {
         $usageFee = new UsageFee();
 
-        // ユーザー数の境界値分析テスト
-        $this->assertEquals(5000, $usageFee->getTotalFee(4, 0));
-        $this->assertEquals(5000, $usageFee->getTotalFee(5, 0));
-        $this->assertEquals(6000, $usageFee->getTotalFee(6, 0));
-        $this->assertEquals(49000, $usageFee->getTotalFee(49, 0));
-        $this->assertEquals(50000, $usageFee->getTotalFee(50, 0));
-        $this->assertEquals(50500, $usageFee->getTotalFee(51, 0));
-
-        // オプション数の境界値分析テスト
-        $this->assertEquals(55000, $usageFee->getTotalFee(1, 2));
-        $this->assertEquals(80000, $usageFee->getTotalFee(1, 3));
-        $this->assertEquals(85000, $usageFee->getTotalFee(1, 4));
+        $this->assertEquals(5000, $usageFee->getUserFee(4));
+        $this->assertEquals(5000, $usageFee->getUserFee(5));
+        $this->assertEquals(6000, $usageFee->getUserFee(6));
+        $this->assertEquals(49000, $usageFee->getUserFee(49));
+        $this->assertEquals(50000, $usageFee->getUserFee(50));
+        $this->assertEquals(50500, $usageFee->getUserFee(51));
     }
 
-    public function test_Sum_UserFee_ReturnTotalFee()
+    public function testOptionFee()
     {
         $usageFee = new UsageFee();
 
-        // ユーザー数の境界値分析テスト
-        $this->assertEquals(0.05, $usageFee->getUserFeeRatio(4, 2023));
-        $this->assertEquals(0.05, $usageFee->getUserFeeRatio(5, 2023));
-        $this->assertEquals(0.06, $usageFee->getUserFeeRatio(6, 2023));
-        $this->assertEquals(0.49, $usageFee->getUserFeeRatio(49, 2023));
-        $this->assertEquals(0.5, $usageFee->getUserFeeRatio(50, 2023));
-        $this->assertEquals(0.505, $usageFee->getUserFeeRatio(51, 2023));
+        $this->assertEquals(0, $usageFee->getOptionFee(-1));
+        $this->assertEquals(0, $usageFee->getOptionFee(0));
+        $this->assertEquals(25000, $usageFee->getOptionFee(1));
+        $this->assertEquals(50000, $usageFee->getOptionFee(2));
+        $this->assertEquals(75000, $usageFee->getOptionFee(3));
+        $this->assertEquals(80000, $usageFee->getOptionFee(4));
+    }
 
-        // 初期費用の境界値分析テスト
-        $this->assertEquals(0.06, $usageFee->getUserFeeRatio(6, 2023));
-        $this->assertEquals(0.02, $usageFee->getUserFeeRatio(6, 2024));
-        $this->assertEquals(0.02, $usageFee->getUserFeeRatio(6, 2025));
+    public function testInitialCost()
+    {
+        $usageFee = new UsageFee();
+
+        $this->assertEquals(100000, $usageFee->getInitialCost(2023));
+        $this->assertEquals(300000, $usageFee->getInitialCost(2024));
+        $this->assertEquals(300000, $usageFee->getInitialCost(2025));
     }
 }
