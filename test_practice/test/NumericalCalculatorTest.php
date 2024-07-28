f4652935c9dd91c7fb174357f29db2dbab51c005
shogo <shogo.ohashi0417@gmail.com>
2024-07-28 15:28:36 +0900

    a

--- test_practice/test/NumericalCalculatorTest.php
+++ test_practice/test/NumericalCalculatorTest.php
@@ -7,9 +7,13 @@ class NumericalCalculatorTest extends TestCase {
 
     public function testGetShapeReturnsNormal()
     {
+        // 準備(Arrange)
         $numericalCalculator = new NumericalCalculator();
+
+        // 実行(Act)
         $result = $numericalCalculator->add(2, 3);
 
+        // 確認(assert)
         $this->assertEquals(5, $result);
     }
 }
